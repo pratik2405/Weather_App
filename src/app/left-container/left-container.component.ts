fa8878528c52c1c08e341a1f5a40c79a7c4111d5
pratik2405 <shinde7388pratik@gmail.com>
2024-11-10 20:07:24 +0530

    Change Name location variable!

--- src/app/left-container/left-container.component.ts
+++ src/app/left-container/left-container.component.ts
@@ -3,11 +3,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCloud } from '@fortawesome/free-solid-svg-icons';
 import { faCloudRain } from '@fortawesome/free-solid-svg-icons';
 import { WetherService } from '../Service/wether.service';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-left-container',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule,FormsModule],
   templateUrl: './left-container.component.html',
   styleUrl: './left-container.component.css'
 })
@@ -16,11 +16,13 @@ export class LeftContainerComponent {
   //Variables for temp summary
   faCloud:any=faCloud;
   faCloudRain:any=faCloudRain;
+  location: string = '';
 
   constructor(public weatherService:WetherService){}
 
-  onSearch(location:string){
-     this.weatherService.cityName=location;
+  onSearch(slocation:string){
+    slocation=this.location;
+     this.weatherService.cityName=slocation;
      this.weatherService.getData();
   }
 
